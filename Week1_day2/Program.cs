9d66ac4efe923fe217985ed1366ff6d38087f547
PensCup87 <bdt9123@gmail.com>
2017-09-26 14:19:02 -0400

    practice with data types

--- Week1_day2/Program.cs
+++ Week1_day2/Program.cs
@@ -10,6 +10,26 @@ namespace Week1_day2
     {
         static void Main(string[] args)
         {
+
+            char firstInitial = 'B';
+            bool istired = false;
+            bool hasDog = true;
+
+            //other number data types
+            //FLOATS end with f
+            float pi= 3.14f;
+
+            //DECIMALS end with m
+            decimal partialNum = 3.1416293m;
+
+            //Holds more decimals than float and double
+            //Use the type with least decimal places to have a program run more efficitent
+            double someNumber = 2.16440d;
+
+            Console.WriteLine(pi);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNumber);
+
         }
     }
 }
